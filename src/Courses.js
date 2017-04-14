1d9ae31b204afa1727e9eff954809fcf093221e1
Redrain <redrainyday@gmail.com>
2017-04-14 18:33:54 -0500

    courses

--- src/Courses.js
+++ src/Courses.js
@@ -2,72 +2,87 @@ import React, {Component} from "react";
 import {Card, CardActions, CardHeader} from "material-ui/Card";
 import FlatButton from "material-ui/FlatButton";
 import {List, ListItem} from "material-ui/List";
-import Checkbox from "material-ui/Checkbox";
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+
+const tableData = [
+    {
+        id:'CS545',
+        name: 'Web Application Architecture',
+        prereqCourse:'Web Programming',
+        entryBlock:'June 2017 - B2 B4',
+        faculty: 'Arrocha, Xing',
+        selected: true,
+    },
+    {
+        id:'CS435',
+        name: 'Algorithms',
+        prereqCourse:'n/a',
+        entryBlock:'June 2017 - B1 B2 B3 B4',
+        faculty: 'Ruby, Xing, Li',
+    },
+];
 
 class Courses extends Component {
-  render() {
-    return (
-      <div style={styles.container}>
-        <Card>
-          <CardHeader
-            title="Courses"
-          />
-          <div>
-            <List>
-              <ListItem>
-                <div style={styles.listItem}>
-                  <div>
-                    Course Number
-                  </div>
-                  <div>
-                    Course Name
-                  </div>
-                  <div>
-                    Prerequisites
-                  </div>
-                  <div>
-                    target entry blocks
-                  </div>
-                  <div>
-                    faculty
-                  </div>
-                </div>
-              </ListItem>
-              <ListItem
-                leftCheckbox={<Checkbox />}
-                primaryText="2  Course2"
-              />
-              <ListItem
-                leftCheckbox={<Checkbox />}
-                primaryText="3  Course3"
-              />
-            </List>
 
-          </div>
+    render() {
+        return (
+            <div style={styles.container}>
+                <Card>
+                    <CardHeader style={styles.header}
+                        title="Courses"
+                    />
+                    <Table>
+                        <TableHeader
+                            height={'300px'}
+                            fixedHeader={true}
+                            fixedFooter={false}
+                            selectable={true}
+                            multiSelectable={true}
+                        >
+                            <TableRow>
+                                <TableHeaderColumn>ID</TableHeaderColumn>
+                                <TableHeaderColumn>Name</TableHeaderColumn>
+                                <TableHeaderColumn>Prereq Courses</TableHeaderColumn>
+                                <TableHeaderColumn>Entry & Blocks</TableHeaderColumn>
+                                <TableHeaderColumn>Faculty</TableHeaderColumn>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {tableData.map( (row, index) => (
+                                <TableRow key={index} selected={row.selected}>
+                                    <TableRowColumn>{row.id}</TableRowColumn>
+                                    <TableRowColumn>{row.name}</TableRowColumn>
+                                    <TableRowColumn>{row.prereqCourse}</TableRowColumn>
+                                    <TableRowColumn>{row.entryBlock}</TableRowColumn>
+                                    <TableRowColumn>{row.faculty}</TableRowColumn>
+                                </TableRow>
+                            ))}
 
-          <CardActions>
-            <FlatButton label="Delete"/>
-            <FlatButton label="Create"/>
-          </CardActions>
-        </Card>
-      </div>
-    )
-  }
+                        </TableBody>
+                    </Table>
+
+                    <CardActions>
+                        <FlatButton label="Delete"/>
+                        <FlatButton label="Create"/>
+                    </CardActions>
+                </Card>
+            </div>
+        )
+    }
 }
 
 var styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center',
-    paddingTop: '2%',
-  },
-  listItem: {
-    width:'60vw',
-    display: 'flex',
-    flexDirection: 'row',
-  },
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingTop: '2%',
+    },
+    header: {
+        fontSize: '',
+    }
+
 }
 
 export default Courses;
