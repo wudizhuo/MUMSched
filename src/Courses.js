0e872342ce7cdc9480d7cc741f3804e8a28499df
wudizhuo <sunzhuo1228@126.com>
2017-04-14 21:18:14 -0500

    format code

--- src/Courses.js
+++ src/Courses.js
@@ -1,87 +1,86 @@
 import React, {Component} from "react";
 import {Card, CardActions, CardHeader} from "material-ui/Card";
 import FlatButton from "material-ui/FlatButton";
-import {List, ListItem} from "material-ui/List";
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from "material-ui/Table";
 
 const tableData = [
-    {
-        id:'CS545',
-        name: 'Web Application Architecture',
-        prereqCourse:'Web Programming',
-        entryBlock:'June 2017 - B2 B4',
-        faculty: 'Arrocha, Xing',
-        selected: true,
-    },
-    {
-        id:'CS435',
-        name: 'Algorithms',
-        prereqCourse:'n/a',
-        entryBlock:'June 2017 - B1 B2 B3 B4',
-        faculty: 'Ruby, Xing, Li',
-    },
+  {
+    id: 'CS545',
+    name: 'Web Application Architecture',
+    prereqCourse: 'Web Programming',
+    entryBlock: 'June 2017 - B2 B4',
+    faculty: 'Arrocha, Xing',
+    selected: true,
+  },
+  {
+    id: 'CS435',
+    name: 'Algorithms',
+    prereqCourse: 'n/a',
+    entryBlock: 'June 2017 - B1 B2 B3 B4',
+    faculty: 'Ruby, Xing, Li',
+  },
 ];
 
 class Courses extends Component {
 
-    render() {
-        return (
-            <div style={styles.container}>
-                <Card>
-                    <CardHeader style={styles.header}
-                        title="Courses"
-                    />
-                    <Table>
-                        <TableHeader
-                            height={'300px'}
-                            fixedHeader={true}
-                            fixedFooter={false}
-                            selectable={true}
-                            multiSelectable={true}
-                        >
-                            <TableRow>
-                                <TableHeaderColumn>ID</TableHeaderColumn>
-                                <TableHeaderColumn>Name</TableHeaderColumn>
-                                <TableHeaderColumn>Prereq Courses</TableHeaderColumn>
-                                <TableHeaderColumn>Entry & Blocks</TableHeaderColumn>
-                                <TableHeaderColumn>Faculty</TableHeaderColumn>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {tableData.map( (row, index) => (
-                                <TableRow key={index} selected={row.selected}>
-                                    <TableRowColumn>{row.id}</TableRowColumn>
-                                    <TableRowColumn>{row.name}</TableRowColumn>
-                                    <TableRowColumn>{row.prereqCourse}</TableRowColumn>
-                                    <TableRowColumn>{row.entryBlock}</TableRowColumn>
-                                    <TableRowColumn>{row.faculty}</TableRowColumn>
-                                </TableRow>
-                            ))}
+  render() {
+    return (
+      <div style={styles.container}>
+        <Card>
+          <CardHeader style={styles.header}
+                      title="Courses"
+          />
+          <Table>
+            <TableHeader
+              height={'300px'}
+              fixedHeader={true}
+              fixedFooter={false}
+              selectable={true}
+              multiSelectable={true}
+            >
+              <TableRow>
+                <TableHeaderColumn>ID</TableHeaderColumn>
+                <TableHeaderColumn>Name</TableHeaderColumn>
+                <TableHeaderColumn>Prereq Courses</TableHeaderColumn>
+                <TableHeaderColumn>Entry & Blocks</TableHeaderColumn>
+                <TableHeaderColumn>Faculty</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {tableData.map((row, index) => (
+                <TableRow key={index} selected={row.selected}>
+                  <TableRowColumn>{row.id}</TableRowColumn>
+                  <TableRowColumn>{row.name}</TableRowColumn>
+                  <TableRowColumn>{row.prereqCourse}</TableRowColumn>
+                  <TableRowColumn>{row.entryBlock}</TableRowColumn>
+                  <TableRowColumn>{row.faculty}</TableRowColumn>
+                </TableRow>
+              ))}
 
-                        </TableBody>
-                    </Table>
+            </TableBody>
+          </Table>
 
-                    <CardActions>
-                        <FlatButton label="Delete"/>
-                        <FlatButton label="Create"/>
-                    </CardActions>
-                </Card>
-            </div>
-        )
-    }
+          <CardActions>
+            <FlatButton label="Delete"/>
+            <FlatButton label="Create"/>
+          </CardActions>
+        </Card>
+      </div>
+    )
+  }
 }
 
 var styles = {
-    container: {
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        alignItems: 'center',
-        paddingTop: '2%',
-    },
-    header: {
-        fontSize: '',
-    }
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: '2%',
+  },
+  header: {
+    fontSize: '',
+  }
 
 }
 
