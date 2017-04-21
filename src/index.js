b94d51262205c6b765c8f23bf8fcf8aac245d9d2
hatc1984 <hatc1984@gmail.com>
2017-04-21 00:51:34 -0500

    Updated

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import Courses from "./pages/Courses";
 import CreateCourse from "./pages/CreateCourse";
 import EditCourses from "./pages/EditCourses";
 import Sections from "./pages/Sections";
+import CreateUser from "./pages/CreateUser";
 import Dashboard from "./pages/Dashboard";
 import {Router, Route, IndexRoute, browserHistory} from "react-router";
 import store from "./store";
@@ -28,6 +29,7 @@ const router = (
         <Route path="/create_course" component={CreateCourse}/>
         <Route path="/edit_course" component={EditCourses}/>
         <Route path="/sections" component={Sections}/>
+        <Route path="/create_user" component={CreateUser}/>
       </Route>
     </Router>
   </Provider>
