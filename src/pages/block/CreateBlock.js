338589cb163963fa0d8a0e9358d4836e4acab3ed
wudizhuo <sunzhuo1228@126.com>
2017-04-25 16:26:14 -0500

    create block

--- src/pages/block/CreateBlock.js
+++ src/pages/block/CreateBlock.js
@@ -5,99 +5,89 @@ import TextField from "material-ui/TextField";
 import axios from "axios";
 import {baseUrl} from "../../Const";
 import {browserHistory} from "react-router";
-import DatePicker from 'material-ui/DatePicker';
-
-let blockName = "";
-let startDate = "";
-let endDate = "";
-let mppStudents = "";
-let fppStudents = "";
+import DatePicker from "material-ui/DatePicker";
 
 class CreateBlock extends Component {
 
-    render() {
-        return (
-            <div style={styles.container}>
-                <Card style={styles.card}>
-                    <CardHeader titleStyle={styles.header} title="Create Block" />
+  render() {
+    return (
+      <div style={styles.container}>
+        <Card style={styles.card}>
+          <CardHeader titleStyle={styles.header} title="Create Block"/>
 
-                    <div style={styles.content}>
-                        <TextField style={styles.content} floatingLabelText="Block Name" ref="blockName" /><br />
-                        <DatePicker style={styles.content} floatingLabelText="Start Date" ref="startDate" mode="landscape" /> <br />
-                        <DatePicker style={styles.content} floatingLabelText="End Date" ref="endDate" mode="landscape" /> <br />
-                        <TextField style={styles.content} floatingLabelText="Total MPP Students" ref="mppStudents" /> <br />
-                        <TextField style={styles.content} floatingLabelText="Total FPP Students" ref="fppStudents" /> <br />
-                    </div>
-                    <CardActions style={styles.cardAction}>
-                        <FlatButton label="Create" primary={true} onClick={this.create.bind(this)} />
-                    </CardActions>
-                </Card>
-            </div>
-        )
-    }
-    create() {
-        blockName = this.refs.blockName.getValue();
-        startDate = this.refs.startDate.getValue();
-        endDate = this.refs.endDate.getValue();
-        mppStudents = this.refs.mppStudents.getValue();
-        fppStudents = this.refs.fppStudents.getValue();
-        console.log(this.state.value);
+          <div style={styles.content}>
+            <TextField style={styles.content} floatingLabelText="SeqNumber" ref="seqNumber"/><br />
+            <TextField style={styles.content} floatingLabelText="Block Name" ref="blockName"/><br />
+            <DatePicker style={styles.content} floatingLabelText="Start Date" ref="startDate" mode="landscape"/> <br />
+            <DatePicker style={styles.content} floatingLabelText="End Date" ref="endDate" mode="landscape"/> <br />
+          </div>
+          <CardActions style={styles.cardAction}>
+            <FlatButton label="Create" primary={true} onClick={this.create.bind(this)}/>
+          </CardActions>
+        </Card>
+      </div>
+    )
+  }
 
-        const url = baseUrl + 'blocks/add';
-        axios.post(url, {
-            blockName: blockName,
-            startDate: startDate,
-            endDate: endDate,
-            totalMPPStudents: mppStudents,
-            totalFPPStudents: fppStudents,
-        })
-            .then(function (response) {
-                //show snack bar
-                console.log(response);
-                browserHistory.push('/blocks');
-            })
-            .catch(function (error) {
-                //show snack bar
-                console.log("error----");
-                console.log(error);
-            });
-    }
+  create() {
+    let seqNumber = this.refs.seqNumber.getValue();
+    let blockName = this.refs.blockName.getValue();
+    let startDate = this.refs.startDate.getDate();
+    let endDate = this.refs.endDate.getDate();
+
+    const url = baseUrl + 'blocks/add';
+    axios.post(url, {
+      "name": blockName,
+      "seqNumber": seqNumber,
+      "startDate": startDate,
+      "endDate": endDate,
+    })
+      .then(function (response) {
+        //show snack bar
+        console.log(response);
+        browserHistory.push('/blocks');
+      })
+      .catch(function (error) {
+        //show snack bar
+        console.log(error);
+      });
+  }
 
 
 }
 
 var styles = {
-    container: {
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        alignItems: 'center',
-        paddingTop: '2%',
-    },
-    card: {
-        width: '60%',
-    },
-    content: {
-        width: '98%',
-        paddingLeft: '2%',
-        fontSize: '14',
-        paddingRight: '2%',
-    },
-    listItem: {
-        width: '60vw',
-        display: 'flex',
-        flexDirection: 'row',
-    },
-    header: {
-        fontSize: '20px',
-    },
-    cardAction: {
-        display: 'flex',
-        justifyContent: 'flex-end',
-    },
-    customWidth: {
-        width: 150,
-    },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: '2%',
+  },
+  card: {
+    width: '60%',
+  },
+  content: {
+    width: '98%',
+    paddingLeft: '2%',
+    fontSize: '14',
+    paddingRight: '2%',
+  },
+  listItem: {
+    width: '60vw',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  header: {
+    fontSize: '20px',
+  },
+  cardAction: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  customWidth: {
+    width: 150,
+  },
 
 }
 
