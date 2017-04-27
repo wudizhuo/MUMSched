a2cacdb0072bbd37d4e993175cd7e5b6b10c4c84
wudizhuo <sunzhuo1228@126.com>
2017-04-27 09:38:48 -0500

    change IP

--- src/Const.js
+++ src/Const.js
@@ -1,6 +1,6 @@
-export const baseUrl = 'http://172.17.7.145:8081/';
-export const baseUrl2 = 'http://172.17.7.145:8082/';
-export const baseUrl3 = 'http://172.17.7.145:8083/';
+export const baseUrl = 'http://10.10.52.10:8081/';
+export const baseUrl2 = 'http://10.10.52.10:8082/';
+export const baseUrl3 = 'http://10.10.52.10:8083/';
 export const role = {
   Student: 'STUDENT',
   Faculty: 'FACULTY',
