fc816f4f453f82e933ac29a7b0d9519cd74bf62d
wudizhuo <sunzhuo1228@126.com>
2017-04-27 10:48:40 -0500

    change IP

--- src/Const.js
+++ src/Const.js
@@ -1,8 +1,10 @@
-export const baseUrl = 'http://10.10.52.10:8081/';
-export const baseUrl2 = 'http://10.10.52.10:8082/';
-export const baseUrl3 = 'http://10.10.52.10:8083/';
+export const baseUrl = 'http://10.10.15.89:8081/';
+export const baseUrl2 = 'http://10.10.15.89:8082/';
+export const baseUrl3 = 'http://10.10.15.89:8083/';
 
-export const consorseUrl = baseUrl+'course-service/';
+// export const consorseUrl = baseUrl+'course-service/';
+export const consorseUrl = baseUrl;
+
 export const role = {
   Student: 'STUDENT',
   Faculty: 'FACULTY',
